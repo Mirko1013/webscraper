ebbfc73180dbd3a972bcc1bbe7a36029f4fb65ba
Mirko <76791414@qq.com>
2019-08-28 17:39:00 +0800

    新增queue类实现

--- Crawler/sitemap/Queue.py
+++ Crawler/sitemap/Queue.py
@@ -4,3 +4,26 @@
 # @Author  : mirko
 # @FileName: Queue.py
 # @Software: PyCharm
+
+from queue import Queue
+
+
+class TaskQueue(object):
+
+
+    def __init__(self):
+        self.queue = Queue()
+
+
+
+    def add(self, job):
+        pass
+
+    def canBeAdded(self, job):
+
+
+    def getTaskQueueSize(self):
+        return self.queue.qsize()
+
+
+
